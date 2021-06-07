3cea83031fde958f91719ffa4c72542cab6580f3
menezesmario <mario082@gmail.com>
2021-06-07 14:37:43 -0300

    Open - Version 1.0

--- Program.cs
+++ Program.cs
@@ -31,7 +31,19 @@ namespace TextEditor
 
         static void Open() //3
         {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+            string path = Console.ReadLine();
 
+            using(var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
         }
 
         static void Edit() //2
